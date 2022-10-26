b08945800317bc38b5cb4849c007f04a9b879183
Shwetal <more.shwetal777@gamil.com>
2022-10-26 11:03:34 +0530

    update

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -2,11 +2,27 @@
 Django command to wait for the database to be available.
 """
 
+from sqlite3 import OperationalError
+from tkinter.ttk import Style
 from django.core.management.base import BaseCommand
 
+import time
+
+from psycopg2 import OperationalError as Psycopg2OpError
 
 class Command(BaseCommand):
     """Django command to wait for database"""
 
     def handle(self, *args, **options):
-        pass
+        """Entry point of command."""
+        self.stdout.write("Waiting for database...")
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up =True
+            except (Psycopg2OpError, OperationalError):
+                self.stdout.write('Database unavailable, waiting 1 second ...')
+                time.sleep(1)
+
+        self.stdout.write(self/Style.SUCCESS('Database Available!'))
